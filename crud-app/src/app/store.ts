a78cbe73c0cf8e2577255a690ebd75130e7d9b2f
Tanu sharma <84377106+developer-tanu@users.noreply.github.com>
2022-01-18 10:32:32 +0530

    Add files via upload

--- crud-app/src/app/store.ts
+++ crud-app/src/app/store.ts
@@ -1,17 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { userApi } from "../features/user/userslice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+setupListeners(store.dispatch);
